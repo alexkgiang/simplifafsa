f60feeba124306e51d5d217c490fc2e63e0e4b47
Alex Giang <agiang595@gmail.com>
2023-07-01 12:43:04 -0700

    select fields, textarea, file upload

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
-const puppeteer = require('puppeteer');
-const { user } = require('./user.js');
-const MatchL = require('./matchL.js');
+const puppeteer = require("puppeteer");
+const { user } = require("./user.js");
+const MatchL = require("./matchL.js");
 
 // const URL = "https://wellfound.com/company/via-transportation/jobs/1841647-strategic-finance-associate?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic"
 
-const URL = 'https://www.tesla.com/careers/search/job/apply/117786';
+const URL = "https://www.tesla.com/careers/search/job/apply/117786";
 
 class Form {
   /* Fills out an entire form */
@@ -15,17 +15,24 @@ class Form {
   }
 
   async sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async handleInput(page, fieldName) {
+  async handleInput(page, fieldName, fieldType) {
     var response;
     const element = await page.$(`[name="${fieldName}"]`);
     if (element !== null) {
-      //handle file input here
-      response = await this.match.fillInputField(this.user, fieldName)
-      await page.type(`input[name="${fieldName}"]`, response);
-      await this.sleep(1000);
+      //handle file input
+      if (fieldType === 'text') { //text input
+        response = await this.match.fillInputField(this.user, fieldName);
+        await page.type(`input[name="${fieldName}"]`, response);
+      }
+      else if (fieldType === 'file') {
+        console.log('helo')
+        response = await this.match.findCorrectFile(this.user, fieldName);
+        console.log(response)
+        await element.uploadFile(response);
+      }
     }
   }
 
@@ -33,38 +40,59 @@ class Form {
     var response;
     const element = await page.$(`[name="${fieldName}"]`);
     if (element !== null) {
-        response = await this.match.fillSelectField(this.user, fieldName)
-        await page.select(`[name="${fieldName}"]`, response);
-        await this.sleep(500);
+      response = await this.match.fillSelectField(this.user, fieldName);
+      await page.select(`[name="${fieldName}"]`, response);
+      await this.sleep(500);
+    }
+  }
+
+  async handleTextarea(page, fieldName) {
+    const element = await page.$(`[name="${fieldName}"]`);
+    if (element !== null) {
+      const response = await this.match.fillInputField(this.user, fieldName);
+      await page.type(`textarea[name="${fieldName}"]`, response);
+      await this.sleep(500);
     }
   }
 
   async main() {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    await page.goto(this.url, { waitUntil: 'networkidle2' });
+    await page.goto(this.url, { waitUntil: "networkidle2" });
 
+    // text input, file input
     const inputFields = await page.$$eval('input', fields =>
-      fields.map(field => field.name)
+      fields.map(field => ({ name: field.name, type: field.type }))
     );
 
-    const selectFields = await page.$$eval('select', fields =>
-      fields.map(field => field.name)
+    // dropdown select
+    const selectFields = await page.$$eval("select", (fields) =>
+      fields.map((field) => field.name)
     );
 
-    const buttonFields = await page.$$eval('button', fields =>
-      fields.map(field => field.name)
+    // text area
+    const textareaFields = await page.$$eval("textarea", (fields) =>
+      fields.map((field) => field.name)
+    );
+
+    const buttonFields = await page.$$eval("button", (fields) =>
+      fields.map((field) => field.name)
     );
 
     for (const field of inputFields) {
-      await this.handleInput(page, field);
+      await this.handleInput(page, field.name, field.type);
     }
 
     for (const field of selectFields) {
       await this.handleSelect(page, field);
     }
 
+    for (const field of textareaFields) {
+      await this.handleTextarea(page, field);
+    }
+
+    await this.sleep(2000);
     await browser.close();
   }
 }
--- matchL.js
+++ matchL.js
@@ -1,17 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const { user } = require('./user');
+const { user } = require("./user");
 
 const { OpenAI } = require("langchain/llms/openai");
 const { ChatOpenAI } = require("langchain/chat_models/openai");
-const { PromptTemplate, ChatMessagePromptTemplate, ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate } = require("langchain/prompts");
+const {
+  PromptTemplate,
+  ChatMessagePromptTemplate,
+  ChatPromptTemplate,
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+} = require("langchain/prompts");
 const { StructuredOutputParser } = require("langchain/output_parsers");
 const { HumanChatMessage, SystemChatMessage } = require("langchain/schema");
 
-class ExtendedStructuredOutputParser extends StructuredOutputParser {
+class TextStructuredOutputParser extends StructuredOutputParser {
   getFormatInstructions() {
-    return (
-      `
+    return `
       The output should have the answer to the prompt, and the answer only.
       There should be no punctuation, quotes, or any extra output.
       For example, if the answer is Jane, then the answer should produce: Jane 
@@ -19,40 +24,59 @@ class ExtendedStructuredOutputParser extends StructuredOutputParser {
       For example, a link would be: https://www.linkedin.com/in/alex-giange/
       A phone number would be: 9728443577
       Again, the output should have the answer to the prompt, and nothing else.
-      `
-    );
+      `;
   }
 
   parse(raw) {
     return JSON.parse(raw);
   }
 
-  parseWithPrompt(text, prompt) {
-    // ...
-  }
+  parseWithPrompt(text, prompt) {}
 }
 
+class SelectStructuredOutputParser extends StructuredOutputParser {
+  getFormatInstructions() {
+    return `
+    The output should have the selection for the select field.
+    If the select field has options like "Yes", "No", the output should be one of these options.
+    For example, if the answer is "Yes", then the answer should produce: Yes
+    The output should be the most appropriate option based on the user's information.
+    There should be no punctuation, quotes, or any extra output.
+    Again, the output should have the answer to the prompt, and nothing else.
+    `;
+  }
+
+  parse(raw) {
+    return JSON.parse(raw);
+  }
+
+  parseWithPrompt(text, prompt) {}
+}
 
 class MatchL {
   /* Matches a field to the appropriate answer based on the user */
   constructor() {
-    this.openai = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
+    this.openai = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      temperature: 0,
+    });
   }
 
   async fillInputField(user, inputField) {
-
-    const parser = ExtendedStructuredOutputParser.fromNamesAndDescriptions({
+    const parser = TextStructuredOutputParser.fromNamesAndDescriptions({
       answer: "answer to the user's question",
       source: "source used to answer the user's question",
     });
-    
+
     const formatInstructions = parser.getFormatInstructions();
-    
+
     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
       SystemMessagePromptTemplate.fromTemplate(
         "The user is filling out a form. You are a helpful assistant that helps the user decide what to put into each field. \n{format_instructions}"
       ),
-      HumanMessagePromptTemplate.fromTemplate("The current field is {field}. Here is information about the user: \n{user}"),
+      HumanMessagePromptTemplate.fromTemplate(
+        "The current field is {field}. Here is information about the user: \n{user}"
+      ),
     ]);
 
     const input = await chatPrompt.format({
@@ -61,30 +85,59 @@ class MatchL {
       user: user.toString(),
     });
 
-    const response = await this.openai.call([
-      new HumanChatMessage(input)
-    ]);
+    const response = await this.openai.call([new HumanChatMessage(input)]);
 
     // const parsedResponse = await parser.parse(response)
-    
-    return response.text
+
+    return response.text;
   }
 
   async fillSelectField(user, selectField) {
-    
+    const parser = SelectStructuredOutputParser.fromNamesAndDescriptions({
+      answer: "answer to the user's question",
+      source: "source used to answer the user's question",
+    });
+
+    const formatInstructions = parser.getFormatInstructions();
+
+    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+      SystemMessagePromptTemplate.fromTemplate(
+        "The user is filling out a form. You are a helpful assistant that helps the user decide what to put into each field. \n{format_instructions}"
+      ),
+      HumanMessagePromptTemplate.fromTemplate(
+        "The current field is {field}. Here is information about the user: \n{user}"
+      ),
+    ]);
+
+    const input = await chatPrompt.format({
+      format_instructions: formatInstructions,
+      field: selectField,
+      user: user.toString(),
+    });
+
+    const response = await this.openai.call([new HumanChatMessage(input)]);
+
+    // const parsedResponse = await parser.parse(response)
+
+    return response.text;
+  }
+
+  async findCorrectFile(user, selectField) {
+    const path = require('path');
+    const filePath = path.resolve(__dirname, 'data', `AlexGiangResume.pdf`);
+
+    return filePath;
   }
 }
 
-async function run() {
+// async function run() {
+//   const match = new MatchL();
 
-  const match = new MatchL();
+//   const response = await match.fillInputField(user, "first name");
 
-  const response = await match.fillInputField(user, "first name");
+//   console.log(response);
+// }
 
-  console.log(response)
-}
-
-run();
-
+// run();
 
 module.exports = MatchL;
